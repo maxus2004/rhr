00adc452457faa494e32cba8804529474d442359
Maksim Doroshin <m1doroshin@email.com>
2025-10-29 22:07:24 +0300

    asd

--- lidar.py
+++ lidar.py
@@ -16,7 +16,7 @@ lidar_distances = [0.0]*360
 def imu_thread():
     global lidar_distances_tmp, lidar_distances
     for line in imu_process.stdout:
-        print("LIDAR > ", line.strip())
+        # print("LIDAR > ", line.strip())
         if(line.startswith("bin[")):
             match = re.search(r"bin\[(\d+)\].*?range=([\d.]+)", line)
             deg = int(match.group(1))
@@ -24,7 +24,7 @@ def imu_thread():
             lidar_distances_tmp[deg] = range
         elif(line.startswith("=== end scan ===")):
             lidar_distances = lidar_distances_tmp.copy()
-            print(lidar_distances)
+            # print(lidar_distances)
        
 
 threading.Thread(target=imu_thread).start()
