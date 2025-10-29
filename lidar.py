bcd14ddb39fbb2e934a04879f9e86321b595a2cf
Maksim Doroshin <m1doroshin@email.com>
2025-10-29 20:09:14 +0300

    qwre

--- lidar.py
+++ lidar.py
@@ -1,17 +1,17 @@
 import subprocess
 import threading
 
-imu_process = subprocess.Popen(
-    ["lidar/build/lidar", "--port", "/dev/ttyUSB0", "--baud", "230400", "--print"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    text=True
-)
+# imu_process = subprocess.Popen(
+#     ["lidar/build/lidar", "--port", "/dev/ttyUSB0", "--baud", "230400", "--print"],
+#     stdout=subprocess.PIPE,
+#     stderr=subprocess.STDOUT,
+#     text=True
+# )
 
-def imu_thread():
-    global imu_yaw
-    for line in imu_process.stdout:
-        print("LIDAR > ", line.strip())
+# def imu_thread():
+#     global imu_yaw
+#     for line in imu_process.stdout:
+#         print("LIDAR > ", line.strip())
        
 
 # threading.Thread(target=imu_thread).start()
