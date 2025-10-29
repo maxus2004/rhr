45bef9f078d9751d63562689f6252cce1492b7c4
Maksim Doroshin <m1doroshin@email.com>
2025-10-29 20:44:54 +0300

    asd

--- lidar.py
+++ lidar.py
@@ -16,7 +16,7 @@ lidar_distances = [0.0]*360
 def imu_thread():
     global lidar_distances_tmp, lidar_distances
     for line in imu_process.stdout:
-        print("LIDAR > ", line.strip())
+        # print("LIDAR > ", line.strip())
         if(line.startswith("bin[")):
             match = re.search(r"bin\[(\d+)\].*?range=([\d.]+)", line)
             deg = int(match.group(1))
@@ -24,10 +24,21 @@ def imu_thread():
             lidar_distances_tmp[deg] = range
         elif(line.startswith("=== end scan ===")):
             lidar_distances = lidar_distances_tmp.copy()
-            print(lidar_distances)
+            # print(lidar_distances)
        
 
 threading.Thread(target=imu_thread).start()
 
 def getDistances():
-    return 0,0,0
+    slice_size = 20
+    front_slice = lidar_distances[0:slice_size] + lidar_distances[359-slice_size:359]
+    right_slice = lidar_distances[90-slice_size:90+slice_size]
+    left_slice = lidar_distances[270-slice_size:270+slice_size]
+    front_slice = [x for x in front_slice if x > 0]
+    right_slice = [x for x in right_slice if x > 0]
+    left_slice = [x for x in left_slice if x > 0]
+    front = sum(front_slice)/len(front_slice)
+    right = sum(right_slice)/len(right_slice)
+    left = sum(left_slice)/len(left_slice)
+    print(left, ", ",front,", ", right)
+    return left,front,right
