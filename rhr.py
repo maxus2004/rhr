7e78a207f38a1a76c38390232dd1953c5e68cd65
Maksim Doroshin <m1doroshin@email.com>
2025-10-29 22:39:47 +0300

    asd

--- rhr.py
+++ rhr.py
@@ -65,7 +65,7 @@ while True:
     leftDistance, frontDistance, rightDistance = lidar.getDistances()
 
     # duh
-    if rightDistance > 0.5:
+    if rightDistance > 0.4:
         # drive forward a bit
         startTime = time.time()
         while(time.time()-startTime < 3):
