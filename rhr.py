e4cc12e66fa2ca8503d3cb39f9d12dd65137bc38
Maksim Doroshin <m1doroshin@email.com>
2025-11-01 10:43:49 +0300

    qwe

--- rhr.py
+++ rhr.py
@@ -68,24 +68,17 @@ print("done calibrating")
 while True:
     leftDistance, frontDistance, rightDistance = lidar.getDistances()
 
-    # duh
-    if rightDistance > RIGHT_DISTANCE:
-        # drive forward a bit
-        startTime = time.time()
-        while(time.time()-startTime < RIGHT_TURN_DELAY):
-            continueDriving()
-            time.sleep(DRIVE_INTERVAL)
-        # if still can turn right, turn right
-        print("TURNING RIGHT")
-        leftDistance, frontDistance, rightDistance = lidar.getDistances()
-        if rightDistance > RIGHT_DISTANCE: turnRight()
-        stop()
-        time.sleep(TURN_DELAY)
-    elif frontDistance < FRONT_DISTANCE:
-        print("TURNING LEFT")
-        turnLeft()
-        stop()
-        time.sleep(TURN_DELAY)
+    if frontDistance < FRONT_DISTANCE:
+        if(leftDistance > rightDistance):
+            print("TURNING LEFT")
+            turnLeft()
+            stop()
+            time.sleep(TURN_DELAY)
+        else:
+            print("TURNING RIGHT")
+            turnRight()
+            stop()
+            time.sleep(TURN_DELAY)
 
     continueDriving()
     time.sleep(DRIVE_INTERVAL)
