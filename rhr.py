d63e7f31b2f827ab67eb07da036c23657058b22a
Maksim Doroshin <m1doroshin@email.com>
2025-11-01 11:19:26 +0300

    a

--- rhr.py
+++ rhr.py
@@ -69,21 +69,21 @@ while True:
     leftDistance, frontDistance, rightDistance = lidar.getDistances()
 
     # duh
-    if leftDistance > LEFT_DISTANCE:
+    if rightDistance > RIGHT_DISTANCE:
         # drive forward a bit
         startTime = time.time()
-        while(time.time()-startTime < LEFT_TURN_DELAY):
+        while(time.time()-startTime < RIGHT_TURN_DELAY):
             continueDriving()
             time.sleep(DRIVE_INTERVAL)
-        # if still can turn left, turn left
-        print("TURNING LEFT")
+        # if still can turn right, turn right
+        print("TURNING RIGHT")
         leftDistance, frontDistance, rightDistance = lidar.getDistances()
-        if leftDistance > LEFT_DISTANCE: turnLeft()
+        if rightDistance > RIGHT_DISTANCE: turnRight()
         stop()
         time.sleep(TURN_DELAY)
     elif frontDistance < FRONT_DISTANCE:
-        print("TURNING RIGHT")
-        turnRight()
+        print("TURNING LEFT")
+        turnLeft()
         stop()
         time.sleep(TURN_DELAY)
 
